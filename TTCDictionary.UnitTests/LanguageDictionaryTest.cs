62526615e52f214b8c17adac2b5b0f744c3ad4ee
fermonas <fermonast@gmail.com>
2017-09-14 20:52:24 +0200

    interview 2

---
+++ TTCDictionary.UnitTests/LanguageDictionaryTest.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+using NUnit.Framework.Internal;
+
+namespace TTCDictionary.UnitTests
+{
+    [TestFixture]
+    public class LanguageDictionaryTest
+    {
+        private LanguageDictionary SUT;
+
+        private Dictionary<string, List<string>> list;
+
+        [SetUp]
+        public void Setup()
+        {
+            list = new Dictionary<string, List<string>>();
+
+            SUT = new LanguageDictionary(list);
+        }
+
+        [Test]
+        public void When_adding_a_word_which_does_not_exist_should_return_true()
+        {
+            // Arrange.
+            var word = "test";
+            var lang = "English";
+
+            // Act.
+            var result = this.SUT.Add(lang, word);
+
+            // Assert.
+            Assert.IsTrue(result);
+
+            var listCheck = this.list.FirstOrDefault(i => i.Key == lang && i.Value.IndexOf(word) >= 0);
+
+            Assert.IsTrue(listCheck.Key == lang);
+            Assert.IsTrue(listCheck.Value.IndexOf(word) >= 0);
+        }
+
+        [Test]
+        public void When_adding_a_word_which_does_exist_should_return_false()
+        {
+            // Arrange.
+            var word = "test";
+            this.SUT.Add("English", word);
+
+            // Act.
+            var result = this.SUT.Add("English", word);
+
+            // Assert.
+            Assert.IsFalse(result);
+        }
+
+        [Test]
+        public void When_adding_a_word_which_does_exist_but_in_a_different_language_should_return_true()
+        {
+            // Arrange.
+            var word = "test";
+            this.SUT.Add("English", word);
+
+            // Act.
+            var result = this.SUT.Add("German", word);
+
+            // Assert.
+            Assert.IsTrue(result);
+        }
+
+        [Test]
+        public void When_checking_a_word_which_does_not_exist_should_return_false()
+        {
+            // Arrange.
+            var word = "test";
+
+            // Act.
+            var result = this.SUT.Check("English", word);
+
+            // Assert.
+            Assert.IsFalse(result);
+        }
+
+        [Test]
+        public void When_checking_a_word_which_does_exist_should_return_true()
+        {
+            // Arrange.
+            var word = "test";
+            this.SUT.Add("English", word);
+
+            // Act.
+            var result = this.SUT.Check("English", word);
+
+            // Assert.
+            Assert.IsTrue(result);
+        }
+
+        [Test]
+        public void When_searching_all_word_that_starts_with_hi()
+        {
+            // Arrange.
+            List<string> wordsListGood = new List<string>();
+            List<string> wordsList     = new List<string>();
+            wordsListGood.Add("hi" );
+            wordsListGood.Add("hi1");
+            wordsListGood.Add("hi2");
+            wordsListGood.Add("hi3");
+            wordsListGood.Add("hi4");
+            wordsListGood.Add("hi5");
+            wordsListGood.Add("hi6");
+
+            wordsList.Add("hi" );
+            wordsList.Add("hi1");
+            wordsList.Add("hi2");
+            wordsList.Add("hi3");
+            wordsList.Add("hi4");
+            wordsList.Add("hi5");
+            wordsList.Add("hi6");
+
+            wordsList.Add("nothi6");
+            wordsList.Add("nothi7");
+
+            foreach(string s in wordsList)
+            {
+                this.SUT.Add("English", s);
+            }
+
+            // Act.
+            IEnumerable<string> result = this.SUT.Search("hi");
+
+            wordsListGood.Sort();
+
+            Assert.AreEqual(wordsListGood as IEnumerable<string>, result);
+           
+        }
+    }
+}
---
+++ TTCDictionary/LanguageDictionary.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TTCDictionary
+{
+    public class LanguageDictionary : ILanguageDictionary
+    {
+        private Dictionary<string, List<string>> list;
+
+        public LanguageDictionary(Dictionary<string, List<string>> list)
+        {
+            this.list = list;
+        }
+
+        public bool Check(string language, string word)
+        {
+            var listCheck = list.FirstOrDefault(i => i.Key == language && i.Value.IndexOf(word) >= 0);
+            if (listCheck.Key == language && listCheck.Value.IndexOf(word) >= 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+
+        public bool Add(string language, string word)
+        {
+            if (list.Any(i => i.Key == language && i.Value.IndexOf(word) >= 0))
+            {
+                return false;
+            }
+            else if (list.Any(i => i.Key == language))
+            {
+                list[language].Add(word);
+            }
+            else
+            {
+                List<string> l = new List<string>();
+                l.Add(word);
+                list.Add(language, l);
+            }
+
+            return true;
+        }
+
+        public IEnumerable<string> Search(string word) 
+        {
+            int num = list.Count;
+            List<string> finalList = new List<string>();
+            for (int n = 0; n < num; n++)
+            {
+                List<string> value = list.Values.ElementAt(n);
+                IEnumerable<string> iList = value.Where(i => i.StartsWith(word)) as IEnumerable<string>;
+                finalList.AddRange(iList);
+            }
+            finalList.Sort();
+            return finalList as IEnumerable<string>;
+        }
+    }
+}
